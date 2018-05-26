71b8d7be985373d66f2f32837f3b9fc03cdb29c4
Alfonso sempoalt <b7flyzkf@gmail.com>
2018-05-26 01:33:59 -0600

    initial commit

---
+++ csn/snake.js
@@ -0,0 +1,166 @@
+﻿//Variables generales
+var velocidad = 50; // velocidad de la serpiente entre  menos es más y entre mayor el número más lento 
+var tamano = 12; // tamaño de la serpiente
+var puntaje= 0;
+class objeto {
+	constructor(){
+		this.tamano = tamano;
+	}
+	choque(obj){
+		var difx = Math.abs(this.x - obj.x);
+		var dify = Math.abs(this.y - obj.y);
+		if(difx >= 0 && difx < tamano && dify >= 0 && dify < tamano){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+class Cola extends objeto {
+	constructor(x,y){
+		super();
+		this.x = x;
+		this.y = y;
+		this.siguiente = null;
+	}
+	dibujar(ctx){
+		if(this.siguiente != null){
+			this.siguiente.dibujar(ctx);
+		}
+		ctx.fillStyle = "#00FF00"; // color serpiente
+		ctx.fillRect(this.x, this.y, this.tamano, this.tamano);
+	}
+	setxy(x,y){
+		if(this.siguiente != null){
+			this.siguiente.setxy(this.x, this.y);
+		}
+		this.x = x;
+		this.y = y;
+	}
+	meter(){
+		if(this.siguiente == null){
+			this.siguiente = new Cola(this.x, this.y);
+		} else {
+			this.siguiente.meter();
+		}
+	}
+	verSiguiente(){
+		return this.siguiente;
+	}
+}
+
+class Comida extends objeto {
+	constructor(){
+		super();
+		this.x = this.generar();
+		this.y = this.generar();
+	}
+	generar(){
+		var num = (Math.floor(Math.random() * 59))*10;
+		return num;
+	}
+	colocar(){
+		this.x = this.generar();
+		this.y = this.generar();
+	}
+	dibujar(ctx){
+		ctx.fillStyle = "#00FF00"; // color comida
+		ctx.fillRect(this.x, this.y, this.tamano, this.tamano);
+	}
+}
+//Objetos que hay en el juego
+var cabeza = new Cola(20,20);
+var comida = new Comida();
+var ejex = true;
+var ejey = true;
+var xdir = 0;
+var ydir = 0;
+function movimiento(){
+	var nx = cabeza.x+xdir;
+	var ny = cabeza.y+ydir;
+	cabeza.setxy(nx,ny);
+}
+function control(event){
+	var cod = event.keyCode;
+	if(ejex){
+		if(cod == 38){
+			ydir = -tamano;
+			xdir = 0;
+			ejex = false;
+			ejey = true;
+		}
+		if(cod == 40){
+			ydir = tamano;
+			xdir = 0;
+			ejex = false;
+			ejey = true;
+		}
+	}
+	if(ejey){
+		if(cod == 37){
+			ydir = 0;
+			xdir = -tamano;
+			ejey = false;
+			ejex = true;
+		}
+		if(cod == 39){
+			ydir = 0;
+			xdir = tamano;
+			ejey = false;
+			ejex = true;
+		}
+	}
+}
+	 
+function findeJuego(){
+	xdir = 0;
+	ydir = 0;
+	ejex = true;
+	ejey = true;
+	cabeza = new Cola(20,20);
+	comida = new Comida();
+	alert("Has perdido  "+ puntaje);
+}
+function choquepared(){
+	if(cabeza.x < 0 || cabeza.x > 590 || cabeza.y < 0 || cabeza.y > 590){
+		findeJuego();
+	}
+}
+function choquecuerpo(){
+	var temp = null;
+	try{
+		temp = cabeza.verSiguiente().verSiguiente();
+    puntaje+2
+	}catch(err){
+		temp = null;
+	}
+	while(temp != null){
+		if(cabeza.choque(temp)){
+			//fin del juego
+			findeJuego();
+		} else {
+			temp = temp.verSiguiente();
+		}
+	}
+}
+
+function dibujar(){
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+	//aquí va la función principal y todo el juego en general
+	cabeza.dibujar(ctx);
+	comida.dibujar(ctx);
+}
+function main(){
+	choquecuerpo();
+	choquepared();
+	dibujar();
+	movimiento();
+	if(cabeza.choque(comida)){
+		comida.colocar();
+		cabeza.meter();
+	}
+}
+setInterval("main()", velocidad);
